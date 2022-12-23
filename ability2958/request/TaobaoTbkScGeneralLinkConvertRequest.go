bed95b5f2b8b855ab5d3439d8e02a44c1098e1b0
liuda <liuda@xiaoshijie.com>
2022-12-23 15:09:33 +0800

    9.1 淘宝商品id sdk升级

--- ability2958/request/TaobaoTbkScGeneralLinkConvertRequest.go
+++ ability2958/request/TaobaoTbkScGeneralLinkConvertRequest.go
@@ -6,22 +6,22 @@ type TaobaoTbkScGeneralLinkConvertRequest struct {
 	BizSceneId *string `json:"biz_scene_id,omitempty" required:"false" `
 	/*
 	   1-自购省，2-推广赚（代理模式专属ID，代理模式必填，其它模式不用填写本字段）     */
-	PromotionType *int64 `json:"promotion_type,omitempty" required:"false" `
+	PromotionType *string `json:"promotion_type,omitempty" required:"false" `
 	/*
 	   物料列表，可以为url或淘口令     */
-	MaterialList *int64 `json:"material_list,omitempty" required:"false" `
+	MaterialList *string `json:"material_list,omitempty" required:"false" `
 	/*
 	   渠道管理ID（如是主站选品推广场景，必须入参该字段，且bizSceneId字段需入参2-消费者比价场景，否则二次转链失败）     */
-	RelationId *int64 `json:"relation_id,omitempty" required:"false" `
+	RelationId *string `json:"relation_id,omitempty" required:"false" `
 	/*
 	   推广位id，mm_xx_xx_xx pid三段式中的第三段     */
-	AdzoneId *string `json:"adzone_id,omitempty" required:"true" `
+	AdzoneId *int64 `json:"adzone_id,omitempty" required:"true" `
 
 	/* 卖家ID列表 */
 	SellerIdList *string `json:"seller_id_list,omitempty" required:"false" `
 
 	/* 推广位id，mm_xx_xx_xx pid三段式中的第二段 */
-	SiteId *string `json:"site_id" required:"true" `
+	SiteId *int64 `json:"site_id" required:"true" `
 
 	/* 商品ID列表 */
 	ItemIdList *string `json:"item_id_list,omitempty" required:"false" `
@@ -35,22 +35,22 @@ func (req *TaobaoTbkScGeneralLinkConvertRequest) SetBizSceneId(bizSceneId string
 	return req
 }
 
-func (req *TaobaoTbkScGeneralLinkConvertRequest) SetPromotionType(promotionType int64) *TaobaoTbkScGeneralLinkConvertRequest {
+func (req *TaobaoTbkScGeneralLinkConvertRequest) SetPromotionType(promotionType string) *TaobaoTbkScGeneralLinkConvertRequest {
 	req.PromotionType = &promotionType
 	return req
 }
 
-func (req *TaobaoTbkScGeneralLinkConvertRequest) SetMaterialList(materialList int64) *TaobaoTbkScGeneralLinkConvertRequest {
+func (req *TaobaoTbkScGeneralLinkConvertRequest) SetMaterialList(materialList string) *TaobaoTbkScGeneralLinkConvertRequest {
 	req.MaterialList = &materialList
 	return req
 }
 
-func (req *TaobaoTbkScGeneralLinkConvertRequest) SetRelationId(relationId int64) *TaobaoTbkScGeneralLinkConvertRequest {
+func (req *TaobaoTbkScGeneralLinkConvertRequest) SetRelationId(relationId string) *TaobaoTbkScGeneralLinkConvertRequest {
 	req.RelationId = &relationId
 	return req
 }
 
-func (req *TaobaoTbkScGeneralLinkConvertRequest) SetAdzoneId(adzoneId string) *TaobaoTbkScGeneralLinkConvertRequest {
+func (req *TaobaoTbkScGeneralLinkConvertRequest) SetAdzoneId(adzoneId int64) *TaobaoTbkScGeneralLinkConvertRequest {
 	req.AdzoneId = &adzoneId
 	return req
 }
@@ -60,7 +60,7 @@ func (req *TaobaoTbkScGeneralLinkConvertRequest) SetSellerIdList(sellerIdList st
 	return req
 }
 
-func (req *TaobaoTbkScGeneralLinkConvertRequest) SetSiteId(siteId string) *TaobaoTbkScGeneralLinkConvertRequest {
+func (req *TaobaoTbkScGeneralLinkConvertRequest) SetSiteId(siteId int64) *TaobaoTbkScGeneralLinkConvertRequest {
 	req.SiteId = &siteId
 	return req
 }
